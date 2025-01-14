6de7001bca4aa447d24cde75fcd06aa5b3a9341a
arneallwardt <arne.allwardt@th-brandenburg.de>
2025-01-14 11:39:09 +0100

    feat: launch script with tb and open manip (does not work yet)

--- src/warehouse_bot/launch/warehouse_bot_tb_launch.py
+++ src/warehouse_bot/launch/warehouse_bot_tb_launch.py
@@ -1,6 +1,7 @@
 import os
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
@@ -13,12 +14,27 @@ def generate_launch_description():
         'robot.launch.py'
     )
 
+    open_manipulator_controller_launch_file = os.path.join(
+        get_package_share_directory('open_manipulator_x_controller'),
+        'launch',
+        'open_manipulator_x_controller.launch.py'
+    )
 
     return LaunchDescription([ 
 
-        # slam_toolbox
+        DeclareLaunchArgument(
+            'usb_port', default_value='/dev/ttyUSB1', description='USB port for open manipulator'
+        ),
+
+        # turtlebot_bringup
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(turtlebot_bringup_launch_file)
+        ),
+
+        # open_manipulator_x_controller
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(open_manipulator_controller_launch_file),
+            launch_arguments={'usb_port': LaunchConfiguration('usb_port')}.items()
         ),
 
         # image_provider
