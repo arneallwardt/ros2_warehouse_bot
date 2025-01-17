62031d7ec93180428f9c41cc6c2b6fc68b5060ab
arneallwardt <arne.allwardt@th-brandenburg.de>
2025-01-17 16:47:10 +0100

    feat: working product gripper in single node

--- src/warehouse_bot_pilots/warehouse_bot_pilots/product_gripper.py
+++ src/warehouse_bot_pilots/warehouse_bot_pilots/product_gripper.py
@@ -14,7 +14,8 @@ class ProductGripper(Node):
     def __init__(self):
         super().__init__('product_gripper')
         self.holding_product = False
-        self._joint_postion_client = self.create_client(SetJointPosition, '/open_manipulator/goal_joint_space_path')
+        self._arm_client = self.create_client(SetJointPosition, '/open_manipulator/goal_joint_space_path')
+        self._gripper_client = self.create_client(SetJointPosition, '/open_manipulator/goal_tool_control')
         
         self._action_server = ActionServer(
             self,
@@ -40,92 +41,139 @@ class ProductGripper(Node):
             trigger='start_idle', 
             source=['initial', 'grip_product', 'release_product'], 
             dest='idle',
-            after=self.move_arm_to_idle_pose
+            after=self.move_arm_to_goal_pose
         ) 
         self.machine.add_transition(
             trigger='start_hovering_over_product', 
             source='idle', 
-            dest='hover_over_product'
+            dest='hover_over_product',
+            after=self.move_arm_to_goal_pose
         )
         self.machine.add_transition(
             trigger='start_aligning_gripper_with_product', 
             source='hover_over_product', 
-            dest='align_gripper_with_product'
+            dest='align_gripper_with_product',
+            after=self.move_arm_to_goal_pose
         ) 
         self.machine.add_transition(
             trigger='start_gripping_product', 
             source='align_gripper_with_product', 
-            dest='grip_product'
+            dest='grip_product',
+            after=self.adjust_gripper
         ) 
         self.machine.add_transition(
             trigger='start_lowering_product', 
             source='idle', 
-            dest='lower_product'
+            dest='lower_product',
+            after=self.move_arm_to_goal_pose
         )
         self.machine.add_transition(
             trigger='start_releasing_product', 
             source='lower_product', 
-            dest='release_product'
+            dest='release_product',
+            after=self.adjust_gripper
         )
         self.machine.add_transition(
             trigger='error', 
             source='*', 
             dest='error',
             after=self.cancel_action())
+    
 
         self.get_logger().info('product_gripper initialized.')
-        
-    def move_arm_to_pose(self, joint_positions, necessary_state):
-        if self.state != necessary_state: 
-            self.get_logger().info(f"[product_gripper]: wrong state for moving arm to pose. Necessary state: {necessary_state}, current state: {self.state}" ) 
-            self.error()
 
-        self._joint_postion_client.wait_for_service()
+
+    def sendSetJointPositionRequest(self, client, joint_names, joint_positions):
+
+        client.wait_for_service()
         self.get_logger().info('SetJointPosition Client available!')
 
         request = SetJointPosition.Request()
-        request.joint_position.joint_name = ['joint1', 'joint2', 'joint3', 'joint4']
+        request.joint_position.joint_name = joint_names
         request.joint_position.position = joint_positions
         request.path_time = float(os.getenv('PATH_TIME', 2.0))
 
-        joint_pos_future = self._joint_postion_client.call_async(request)
+        joint_pos_future = client.call_async(request)
         rclpy.spin_until_future_complete(self, joint_pos_future)
 
         if joint_pos_future.result():
             time.sleep(float(os.getenv('PATH_TIME', 2.0))) # wait until movement is over
+            
+            # remember if robot is currently holding a product
+            if self.state == 'grip_product':
+                self.holding_product = True
+            elif self.state == 'release_product':
+                self.holding_product = False
+
             self.get_logger().info('Openmanipulator reached its goal pose.')
         else:
             self.get_logger().error('Error while moving openmanipulator to goal pose.')
+
         
+    def move_arm_to_goal_pose(self):
 
-    def move_arm_to_idle_pose(self):
+        if self.state == 'idle':
+            joint_pos_label = 'IDLE'
+        elif self.state == 'hover_over_product':
+            joint_pos_label = 'HOVER'
+        elif self.state == 'align_gripper_with_product':
+            joint_pos_label = 'ALIGN'
+        elif self.state == 'lower_product':
+            joint_pos_label = 'LOWER'
+        else: 
+            self.get_logger().info(f'{self.state} is not a valid state for moving the arm. Entering error pose.')
+            self.error()
+
+        self.get_logger().info(f'Moving arm in state {self.state}')
+
+        joint_names = ['joint1', 'joint2', 'joint3', 'joint4']
         joint_positions = [
-            float(os.getenv('JOINT1_IDLE', 0.0)),
-            float(os.getenv('JOINT2_IDLE', 0.0)), 
-            float(os.getenv('JOINT3_IDLE', 0.0)), 
-            float(os.getenv('JOINT4_IDLE', 0.0))
+            float(os.getenv(f'JOINT1_{joint_pos_label}', 0.0)),
+            float(os.getenv(f'JOINT2_{joint_pos_label}', 0.0)), 
+            float(os.getenv(f'JOINT3_{joint_pos_label}', 0.0)), 
+            float(os.getenv(f'JOINT4_{joint_pos_label}', 0.0))
         ]
-        self.move_arm_to_pose(joint_positions, 'idle')
 
+        self.sendSetJointPositionRequest(
+            client=self._arm_client, 
+            joint_names=joint_names,
+            joint_positions=joint_positions
+        )
 
-    def move_arm_to_hover_over_product(self):
-        joint_positions = [
-            float(os.getenv('JOINT1_HOVER', 0.0)),
-            float(os.getenv('JOINT2_HOVER', 0.0)), 
-            float(os.getenv('JOINT3_HOVER', 0.0)), 
-            float(os.getenv('JOINT4_HOVER', 0.0))
-        ]
-        self.move_arm_to_pose(joint_positions, 'idle')
         
+    def adjust_gripper(self):
+        if self.state == 'grip_product':
+            joint_positions = [-0.01]
+        elif self.state == 'release_product' or 'idle':
+            joint_positions = [0.01]
+        else: 
+            self.get_logger().info(f'{self.state} is not a valid state for adjusting the gripper. Entering error pose.')
+            self.error()
+        
+        self.get_logger().info(f'Moving gripper in state {self.state}')
 
-    def grip_product(self):
+        self.sendSetJointPositionRequest(
+            client=self._gripper_client,
+            joint_names=['gripper'],
+            joint_positions=joint_positions
+        )
+
+        
+    def test_grip_product(self):
+        # make sure to start in idle state
         if not self.state == 'idle':
             self.get_logger().info('Not in idle state, entering idle...')
             self.start_idle()
+        else:
+            self.get_logger().info('bot is in idle state. starting to grip product.')
+            print('already in idle')
         self.start_hovering_over_product()
         self.start_aligning_gripper_with_product()
         self.start_gripping_product()
-
+        self.start_idle()
+        self.start_lowering_product()
+        self.start_releasing_product()
+        self.start_idle()
 
     def grip_product_callback(self, goal_handle):
         self.get_logger().info('def grip_product_callback')
@@ -164,6 +212,7 @@ def main(args=None):
     product_gripper = ProductGripper()
     product_gripper.start_idle()
     print('PRODUCT GRIPPER INSTANTIATED')
+    product_gripper.test_grip_product()
 
     rclpy.spin(product_gripper)
     rclpy.shutdown()    
